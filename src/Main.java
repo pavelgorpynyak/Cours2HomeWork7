164f3e0ded3f98ec9bf2299ba66644ff65feb035
pavel_gorpynyak <pavel_gorpynyak@bk.ru>
2022-08-17 17:31:40 +0300

    Home work is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.*;
+
 public class Main {
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> words = new ArrayList<>(List.of("abc", "bcd", "cde", "abc", "abc","sss","abc", "bcd"));
+
     public static void main( String[] args ) {
-        System.out.println("Hello world!");
+        lesson1();
+        System.out.println();
+        lesson2();
+        System.out.println();
+        lesson3();
+        lesson4();
+
+    }
+
+    public static void lesson1() {
+        for (Integer num : nums) {
+            if (num % 2 != 0) {
+                System.out.print(num);
+            }
+        }
+    }
+
+    public static void lesson2() {
+        Collections.sort(nums);
+        int prevNum = Integer.MIN_VALUE;
+        for (Integer num : nums) {
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.print(num);
+                prevNum = num;
+            }
+        }
+    }
+
+    public static void lesson3() {
+        Set<String> equalWord = new HashSet<>(words);
+        System.out.println(equalWord);
+    }
+
+    public static void lesson4() {
+        Set<String> equalWord = new HashSet<>(words);
+        System.out.println(words.size() - equalWord.size());
     }
 }
+
+
